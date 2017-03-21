4270b80bb9e9a4156eb82d8ef3d5c2fd3374459d
Benji Shankwitz <bshankwitz@gmail.com>
2017-03-21 18:33:52 +0700

    Config setup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "rules": {
+        // spacing -- https://gist.githubusercontent.com/alexnm/402c2355240808177c7c9b9c83bff0a5/raw/6de960abec60741eba4705323d02ddb021ae9580/eslintrc.js
+        "space-in-parens": [ 2, "always" ],
+        "template-curly-spacing": [ 2, "always" ],
+        "array-bracket-spacing": [ 2, "always" ],
+        "object-curly-spacing": [ 2, "always" ],
+        "computed-property-spacing": [ 2, "always" ],
+        "no-multiple-empty-lines": [ 2, { "max": 1, "maxEOF": 0, "maxBOF": 0 } ],
+    },
+
+};
