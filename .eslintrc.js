0415e04020de9b2fc1a03df25db7571927c60c3a
Benji Shankwitz <bshankwitz@gmail.com>
2017-03-30 22:49:58 +0700

    It works, at least

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
         "template-curly-spacing": [ 2, "always" ],
         "array-bracket-spacing": [ 2, "always" ],
         "object-curly-spacing": [ 2, "always" ],
-        "computed-property-spacing": [ 2, "always" ],
+        // "computed-property-spacing": [ 2, "always" ],
         "no-multiple-empty-lines": [ 2, { "max": 1, "maxEOF": 0, "maxBOF": 0 } ],
 
         // code arrangement matter
--- src/deploy.js
+++ src/deploy.js
@@ -1,26 +1,55 @@
 const fs = require("fs")
     , path = require("path")
+    , crc = require("crc").crc32
 
 module.exports = deploy;
 
 function deploy(s3, bucket, directory) {
-  const files = readDirectory(directory);
+  const files = readDirectory(s3, bucket, directory);
   return true;
 }
 
-function readDirectory(directory) {
+function readDirectory(s3, bucket, directory) {
   return fs.readdir(directory, (err, dirList) => {
-    return dirList ? dirList.forEach(handleFile(directory)) : console.log(err);
+    return dirList ? dirList.map(handleFile(s3, bucket, directory)) : console.log(err);
   });
 }
 
-function handleFile(directory) {
+function handleFile(s3, bucket, directory) {
   return (file) => {
     const filePath = path.resolve(directory, file);
-    return fs.statSync(filePath).isDirectory() ? readDirectory(filePath) : print(filePath);
+    return fs.statSync(filePath).isDirectory()
+      ? readDirectory(s3, bucket, filePath)
+      : readFile(s3, bucket, filePath, directory);
   }
 }
 
-function print(string) {
-  return console.log(string);
+function readFile(s3, bucket, filePath, directory) {
+  const file = fs.readFileSync(filePath);
+  const cacheJson = {};
+  try {
+    Object.assign(cacheJson, JSON.parse(fs.readFileSync('your-cache-location')));
+  } catch (err) {
+    {};
+  }
+  return crc(file) === cacheJson[ filePath ]
+    ? console.log('cached:', filePath)
+    : writeCacheFile(s3, bucket, file, filePath, directory, cacheJson);
+}
+
+function writeCacheFile(s3, bucket, file, filePath, directory, cacheJson) {
+  // console.log('deploying:', filePath);
+  const newCache = Object.assign({}, cacheJson, { [filePath]: crc(file) });
+  const cacheFile = fs.writeFileSync('your-cache-location', JSON.stringify(newCache));
+  const projectPath = filePath.split(process.cwd() + "/public/")[1];
+  return deployFile(s3, bucket, projectPath, file);
+}
+
+function deployFile(s3, bucket, projectPath, file) {
+  const params = {
+    Bucket: bucket,
+    Body: file,
+    Key: projectPath
+  }
+  return s3.putObject(params).promise();
 }
